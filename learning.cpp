03ccbc4068d7d7f0c8eddd0e9e5dd043a2d06a07
yangyongguang <30389836+yangyongguang@users.noreply.github.com>
2019-09-19 15:09:01 +0800

    Update learning.cpp
    在远程主机上， 自己的更新

--- learning.cpp
+++ learning.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
 	cout << "first add commit" << endl;
+	cout << "add in origin master after new branch yyg" << endl;
 	return 0;
 }
